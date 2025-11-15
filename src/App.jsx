aa6e5325f30f98f1cc5a470da543815e09fba937
Riann121 <145764804+Riann121@users.noreply.github.com>
2025-11-15 20:52:39 +0600

    add all day-1 code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import {Post} from "./components/Post"
+function App() {
+  return (
+  <div >
+    <Post  author = "rian" body = "dog is my pet"/>
+    <Post  author = "shafayat" body = "o have a cat"/>
+  </div>
+   )
+}
+
+export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+import classes from './post.module.css';
+function Post(props){
+    return(
+    <div className={classes.post}>
+    <p className = {classes.author}>{props.author}</p>
+    <p className= {classes.text}>{props.body}</p>
+    </div>
+    )
+}
+
+export {Post}

fd01ed1bd95b5bffd0f64fbb85a562e6a5b78c66
Riann121 <145764804+Riann121@users.noreply.github.com>
2025-11-21 11:32:25 +0600

    add class 3

---
+++ src/components/Modal.jsx
@@ -0,0 +1,14 @@
+import classes from './Modal.module.css'
+
+function Modal ({children,visibleStatus}){
+    return(
+        <>
+        <div className={classes.backdrop} onClick={visibleStatus}/>
+        <dialog open className={classes.modal}>
+        {children}
+        </dialog>
+        </>
+    )
+}
+
+export default Modal
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react'
 import NewPost from './NewPost.jsx'
 import classes from './PostsList.module.css'
 import { Post } from './Post.jsx'
+import Modal from './modal.jsx'
 const PostsList = ()=> {
+    const [isVisible,setIsVisible] = useState(true)
+    const [enteredBody,setEnteredBodyChange]=useState('')
+    const [enteredAuthor,setEnteredAuthorChange]=useState('')
+
+    function bodyChangeHandler(event){
+        setEnteredBodyChange(event.target.value)
+    }
+    function authorChangeHandler(event){
+        setEnteredAuthorChange(event.target.value)
+    }
+
+    function isVisibleHandler(event){
+        setIsVisible(false)
+    }
+
+    let modal;
+    if(isVisible){
+        modal = <Modal visibleStatus={isVisibleHandler}>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+        </Modal>
+    }
     return(
          <>
-            <NewPost />
+            {modal}
             <ul className ={classes.posts} >
-                <Post  author = "rian" body = "dog is my pet"/>
+                <Post  author = {enteredAuthor} body = {enteredBody}/>
                 <Post  author = "shafayat" body = "o have a cat"/>
             </ul>
         </>
